14a954cdccaffe99d423e751704e0f40a6d056d8
Patrick Holody <patryk.holody@gmail.com>
2019-11-20 21:14:55 +0100

    set router /api/v1/movies and connection to mongoDB

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import { json, urlencoded } from 'body-parser';
+import { json } from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 // @ts-ignore
 import logo from 'asciiart-logo';
+import config from './config/index';
+import movieRouter from './resources/movie/movie.router';
+import mongoose from 'mongoose';
 
 const packageJson = require('../package.json');
 packageJson.font = 'Speed';
@@ -15,17 +18,23 @@ export const app = express();
 app.disable('x-powered-by');
 app.use(cors());
 app.use(json());
+
+// @TODO write logs to the console
+app.use(morgan('dev'));
+// @TODO write logs to the log file
 app.use(
   morgan('combined', {
     stream: require('fs').createWriteStream('app/logs/out.log')
   })
 );
+app.use('/api/v1', movieRouter);
 
 export const start = async () => {
   try {
-    app.listen(5000, () => {
+    await mongoose.connect(config.dbUrl, { useNewUrlParser: true });
+    app.listen(config.port, () => {
       console.log(logo(packageJson).render());
-      console.log(`Node.js start on port: 5000(HTTP)`);
+      console.log(`Node.js start on port: ${config.port}`);
     });
   } catch (e) {
     console.error('There are error in Movies Service: ', e);
