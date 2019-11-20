270c3a2da2d07fef25dc0e0a8642579f5bcafc58
Patrick Holody <patryk.holody@gmail.com>
2019-11-20 01:08:19 +0100

    init project

---
+++ README.md
@@ -0,0 +1,18 @@
+## Instalation:
+
+You need to have [yarn](https://yarnpkg.com/lang/en/) and [node](https://nodejs.org/en/) installed on your machine.
+Download or clone the repo, install all packages needed by running `yarn install`
+
+## Usage:
+
+### Build:
+
+```
+  yarn build
+```
+
+### Dev:
+
+```
+  yarn dev
+```
---
+++ app/server.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import { json, urlencoded } from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import logo from 'asciiart-logo';
+
+const packageJson = require('../package.json');
+packageJson.font = 'Speed';
+packageJson.logoColor = 'bold-green';
+packageJson.textColor = 'green';
+packageJson.borderColor = 'grey';
+
+export const app = express();
+app.disable('x-powered-by');
+app.use(cors());
+app.use(json());
+app.use(
+  morgan('combined', {
+    stream: require('fs').createWriteStream('app/logs/out.log')
+  })
+);
+
+export const start = async () => {
+  try {
+    app.listen(5000, () => {
+      console.log(logo(packageJson).render());
+      console.log(`Node.js start on port: 5000(HTTP)`);
+    });
+  } catch (e) {
+    console.error('There are error in Movies Service: ', e);
+  }
+};
